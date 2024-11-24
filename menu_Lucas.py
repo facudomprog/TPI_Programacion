0b109e9a8c904864be8067f10bf6897959f46059
LucasLag0 <143980543+LucasLag0@users.noreply.github.com>
2024-11-24 20:02:07 -0300

    Menu alternativo, todavia no tiene explicacion de que es un menu

---
+++ menu_Lucas.py
@@ -0,0 +1,13 @@
+while True:
+    op = int(input())
+
+    match op:
+        case 1:
+            import sopaDeLetras
+
+        case 2:
+            import ejemplo
+
+        case 3:
+            import vlackyack
+            vlackyack.blackjack()
